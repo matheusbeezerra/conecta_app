346c691ba35a3e007f9b87af5f068813dfe1de7d
matheusbeezerra <matheusbeezerra96@gmail.com>
2024-04-22 11:25:32 -0300

    up

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,8 +1,8 @@
+import 'package:conecta_app/pages/login/register_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:conecta_app/components/button.dart';
 import 'package:conecta_app/components/campo_input.dart';
-import 'package:conecta_app/pages/register_page.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
