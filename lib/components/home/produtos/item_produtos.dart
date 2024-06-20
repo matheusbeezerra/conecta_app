b6b68c12464aafe8f1dd63394626f94e42774c6c
--replace <jessica.paiva-tc@fundaj.gov.br>
2024-06-20 09:40:31 -0300

    atualização de links externos do grid de produtos na home e mudança de cor no plano de fundo da home para branco

--- lib/components/home/produtos/item_produtos.dart
+++ lib/components/home/produtos/item_produtos.dart
@@ -1,5 +1,6 @@
-import 'package:conecta_app/components/difor/content_modal_difor.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart' as launcher;
+import 'package:conecta_app/components/difor/content_modal_difor.dart';
 import 'package:conecta_app/models/produtos/info_produtos.dart';
 import 'package:conecta_app/models/colors.dart';
 
@@ -14,55 +15,26 @@ class ItemProdutoFundaj extends StatelessWidget {
       textDirection: TextDirection.ltr,
       child: InkWell(
         onTap: () {
-          // Verifica se o item clicado é o de 'Formação'
-          if (produtosFundaj.produto == 'Formação') {
-            // Mostra o bottom sheet quando o item de 'Formação' é clicado
-            showModalBottomSheet(
-              context: context,
-              builder: (context) {
-                return Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(22),
-                      topRight: Radius.circular(22),
-                    ),
-                  ),
-                  height: 300,
-                  width: double.infinity,
-                  child: const Padding(
-                      padding: EdgeInsets.fromLTRB(20, 30, 18, 20),
-                      child: Column(
-                        children: [
-                          Text(
-                            "DIFOR - Diretoria de Formação ",
-                            style: TextStyle(
-                              color: darkGreen,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          Flexible(
-                            //AQUI DENTRO FICA O TEXTO DO RESUMO DAS EXPOSICÕES
-                            // height: 100,
-                            child: SingleChildScrollView(
-                              child: ContentModalDifor(),
-                            ),
-                          ),
-                        ],
-                      )),
-                );
-              },
-            );
-          } else {
-            // Navega para a página correspondente ao produto clicado
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => produtosFundaj.endProduto,
-              ),
-            );
+          switch (produtosFundaj.produto) {
+            case 'Formação':
+              _showFormacaoBottomSheet(context);
+              break;
+            case 'PIBIC':
+              _launchURL(context, 'https://www.gov.br/fundaj/pt-br/composicao/dipes-1/pibic');
+              break;
+            case 'Pesquisa\nEscolar':
+              _launchURL(context, 'https://pesquisaescolar.fundaj.gov.br/pt-br/');
+              break;
+            case 'Villa\nDigital':
+              _launchURL(context, 'https://villadigital.fundaj.gov.br/');
+              break;
+            default:
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => produtosFundaj.endProduto,
+                ),
+              );
           }
         },
         child: Column(
@@ -89,5 +61,68 @@ class ItemProdutoFundaj extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  // Função para mostrar o Bottom Sheet de Formação
+  void _showFormacaoBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(22),
+              topRight: Radius.circular(22),
+            ),
+          ),
+          height: 300,
+          width: double.infinity,
+          child: const Padding(
+            padding: EdgeInsets.fromLTRB(20, 30, 18, 20),
+            child: Column(
+              children: [
+                Text(
+                  "DIFOR - Diretoria de Formação ",
+                  style: TextStyle(
+                    color: darkGreen,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Flexible(
+                  child: SingleChildScrollView(
+                    child: ContentModalDifor(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  // Função para tentar lançar o URL
+  void _launchURL(BuildContext context, String url) {
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
+    launcher.canLaunch(url).then((canLaunch) {
+      if (canLaunch) {
+        launcher.launch(url);
+      } else {
+        scaffoldMessenger.showSnackBar(
+          SnackBar(
+            content: Text('Não foi possível abrir $url'),
+          ),
+        );
+      }
+    }).catchError((error) {
+      scaffoldMessenger.showSnackBar(
+        SnackBar(
+          content: Text('Erro ao abrir $url: $error'),
+        ),
+      );
+    });
   }
 }
